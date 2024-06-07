10570d33101be32b8d282f59e075ec838d551dd4
manubharucha <manubharucha1631@gmail.com>
2024-06-07 17:56:06 -0400

    Created product class and set up nUnit test environment

---
+++ Assignment2(group12)/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Assignment2_group12
+{
+   public class Product
+        {
+            public int ProductID { get; set; }
+            public string ProductName { get; set; }
+            public decimal Price { get; set; }
+            public int Stock { get; set; }
+
+            public Product(int productId, string productName, decimal price, int stock)
+            {
+                ProductID = productId;
+                ProductName = productName;
+                Price = price;
+                Stock = stock;
+            }
+
+            public void IncreaseStock(int amount)
+            {
+                Stock += amount;
+            }
+
+            public void DecreaseStock(int amount)
+            {
+                if (Stock - amount < 0)
+                {
+                    throw new InvalidOperationException("Insufficient stock.");
+                }
+                Stock -= amount;
+            }
+        }
+    }
+
+
---
+++ Assignment2(group12)Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+using Assignment2_group12;
+
+namespace Assignment2_group12.Tests
+{
+    [TestFixture]
+    public class ProductTest
+    {
+       
+    }
+}
